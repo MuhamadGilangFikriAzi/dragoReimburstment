95bb2b137a914a621b6b8590e6f9b42ab001dc72
gilang9802 <mgfa9802@gmail.com>
2020-05-19 09:09:26 +0700

    add footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -56,29 +56,29 @@
 
 </head>
 <body class="hold-transition sidebar-mini layout-fixed" style="height: auto;">
-  <div class="wrapper">
-    <div id="app">
-      <!-- Navbar -->
-      <nav class="main-header navbar navbar-expand navbar-dark navbar-light">
-        <!-- Left navbar links -->
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-          </li>
-        </ul>
-        <!-- Right navbar links -->
-        <ul class="navbar-nav ml-auto">
-           <!-- Authentication Links -->
-            @guest
-             <li class="nav-item">
-                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-            </li>
-            @if (Route::has('register'))
-             <li class="nav-item">
-               <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-            </li>
-            @endif
-            @else
+    <div class="wrapper">
+        <div id="app">
+        <!-- Navbar -->
+            <nav class="main-header navbar navbar-expand navbar-dark navbar-light">
+                <!-- Left navbar links -->
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+                    </li>
+                </ul>
+                <!-- Right navbar links -->
+                <ul class="navbar-nav ml-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                    @else
           <!-- Messages Dropdown Menu -->
           {{-- <li class="nav-item dropdown">
             <div class="user-panel d-flex">
@@ -97,62 +97,63 @@
               {{ Auth::user()->name }}
             </a>
           </li> --}}
-          @endguest
-        </ul>
-      </nav>
+                    @endguest
+                </ul>
+            </nav>
 
-      <!-- Main Sidebar Container -->
-      <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <!-- Brand Logo -->
-        <a href="#" class="brand-link">
-          <img src="{{asset ('adminlte/dist/img/AdminLTELogo.png')}}" alt="img" class="brand-image img-circle elevation-3"
-               style="opacity: .8">
-          <span class="brand-text font-weight-light">Reimbursement</span>
-        </a>
-
-        <!-- Sidebar -->
-        <div class="sidebar">
-          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-              @if (Auth::user()->foto != null)
-                <img src="{{asset('img/user/'.Auth::user()->foto)}}" class="img-circle elevation-2" alt="User Image">
-              @else
-                <img src="{{asset('img/user/user.png')}}" class="img-circle elevation-2" alt="User Image">
-              @endif
-            </div>
-            <a class="nav-link" href="{{ route('home.edit', Auth::user()->id) }}" title="Edit Profile">
-              {{ Auth::user()->name }}
-            </a>
-            <div class="info">
-              <a href="#" class="d-block"></a>
-            </div>
-          </div>
-
-          <!-- Sidebar Menu -->
-          <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-              <!-- Add icons to the links using the .nav-icon class
-                   with font-awesome or any other icon font library -->
-              <li class="nav-item {{ (request()->is('home/*')) ? 'menu-open' : '' }}">
-                <a href="{{ url('/home') }}" class="nav-link {{ (request()->is('home*')) ? 'active' : '' }}">
-                  <i class="nav-icon fas fa-tachometer-alt"></i>
-                  <p>
-                    Dashboard
-                  </p>
+            <!-- Main Sidebar Container -->
+            <aside class="main-sidebar sidebar-dark-primary elevation-4">
+                <!-- Brand Logo -->
+                <a href="#" class="brand-link">
+                    <img src="{{asset ('adminlte/dist/img/AdminLTELogo.png')}}" alt="img" class="brand-image img-circle elevation-3"
+                    style="opacity: .8">
+                    <span class="brand-text font-weight-light">Reimbursement</span>
                 </a>
 
-                <li class="nav-item {{ (request()->is('reimburstment/*')) ? 'menu-open' : '' }}">
-                    <a href="{{ route('reimburstment.index') }}" class="nav-link {{ (request()->is('reimburstment*')) ? 'active' : '' }}">
-                        <i class="fas fa-hand-holding-usd nav-icon"></i>
-                      <p>Reimbursement</p>
-                    </a>
-                </li>
-                <li class="nav-item {{ (request()->is('pengembalian/*')) ? 'menu-open' : '' }}">
-                    <a href="{{ route('pengembalian.index') }}" class="nav-link {{ (request()->is('pengembalian*')) ? 'active' : '' }}">
-                        <i class="fas fa-hand-holding-usd nav-icon"></i>
-                      <p>Pengembalian Data</p>
-                    </a>
-                </li>
+                <!-- Sidebar -->
+                <div class="sidebar">
+                    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                        <div class="image">
+                            @if (Auth::user()->foto != null)
+                                <img src="{{asset('img/user/'.Auth::user()->foto)}}" class="img-circle elevation-2" alt="User Image">
+                            @else
+                                <img src="{{asset('img/user/user.png')}}" class="img-circle elevation-2" alt="User Image">
+                            @endif
+                        </div>
+                        <a class="nav-link" href="{{ route('home.edit', Auth::user()->id) }}" title="Edit Profile">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="info">
+                            <a href="#" class="d-block"></a>
+                        </div>
+                    </div>
+
+                    <!-- Sidebar Menu -->
+                    <nav class="mt-2">
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                            <!-- Add icons to the links using the .nav-icon class
+                            with font-awesome or any other icon font library -->
+                            <li class="nav-item {{ (request()->is('home/*')) ? 'menu-open' : '' }}">
+                                <a href="{{ url('/home') }}" class="nav-link {{ (request()->is('home*')) ? 'active' : '' }}">
+                                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                                    <p>
+                                        Dashboard
+                                    </p>
+                                </a>
+                            </li>
+
+                            <li class="nav-item {{ (request()->is('reimburstment/*')) ? 'menu-open' : '' }}">
+                                <a href="{{ route('reimburstment.index') }}" class="nav-link {{ (request()->is('reimburstment*')) ? 'active' : '' }}">
+                                    <i class="fas fa-hand-holding-usd nav-icon"></i>
+                                    <p>Reimbursement</p>
+                                </a>
+                            </li>
+                            <li class="nav-item {{ (request()->is('pengembalian/*')) ? 'menu-open' : '' }}">
+                                <a href="{{ route('pengembalian.index') }}" class="nav-link {{ (request()->is('pengembalian*')) ? 'active' : '' }}">
+                                    <i class="fas fa-hand-holding-usd nav-icon"></i>
+                                    <p>Pengembalian Data</p>
+                                </a>
+                            </li>
 
                 {{-- @hasanyrole('Super Admin|Admin')
                 <li class="nav-item has-treeview {{ (request()->is('report*')) ? 'menu-open' : '' }}">
@@ -179,93 +180,102 @@
                     </ul>
                 </li>
                 @endhasanyrole --}}
-                @hasanyrole('Super Admin|Admin')
-                <li class="nav-item has-treeview {{ (request()->is('report*')) ? 'menu-open' : '' }}">
-                    <a href="#" class="nav-link {{ (request()->is('report*')) ? 'active' : '' }}">
-                      <i class="nav-icon fas fa-list-ul"></i>
-                      <p>
-                        Laporan
-                        <i class="fas fa-angle-left right"></i>
-                      </p>
-                    </a>
-                    <ul class="nav-item nav-treeview">
-                        <li class="nav-item">
-                            <a href="{{ route('report.reimburstment.index') }}" class="nav-link {{ (request()->is('report/reimburstment*')) ? 'active' : '' }}">
-                                <i class="fas fa-clipboard-list nav-icon"></i>
-                                <p>Reimburstment</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="{{ route('report.pengembalian.index') }}" class="nav-link {{ (request()->is('report/pengembalian*')) ? 'active' : '' }}">
-                                <i class="fas fa-clipboard-list nav-icon"></i>
-                                <p>Pengembalan dana</p>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                @endhasanyrole
+                            @hasanyrole('Super Admin|Admin')
+                            <li class="nav-item has-treeview {{ (request()->is('report*')) ? 'menu-open' : '' }}">
+                                <a href="#" class="nav-link {{ (request()->is('report*')) ? 'active' : '' }}">
+                                    <i class="nav-icon fas fa-list-ul"></i>
+                                    <p>
+                                        Laporan
+                                        <i class="fas fa-angle-left right"></i>
+                                    </p>
+                                </a>
+                                <ul class="nav-item nav-treeview">
+                                    <li class="nav-item">
+                                        <a href="{{ route('report.reimburstment.index') }}" class="nav-link {{ (request()->is('report/reimburstment*')) ? 'active' : '' }}">
+                                            <i class="fas fa-clipboard-list nav-icon"></i>
+                                            <p>Reimburstment</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('report.pengembalian.index') }}" class="nav-link {{ (request()->is('report/pengembalian*')) ? 'active' : '' }}">
+                                            <i class="fas fa-clipboard-list nav-icon"></i>
+                                            <p>Pengembalan dana</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            @endhasanyrole
 
-                @hasanyrole('Super Admin|Admin')
-                <li class="nav-item has-treeview {{ (request()->is('settings*')) ? 'menu-open' : '' }}">
-                    <a href="#" class="nav-link {{ (request()->is('settings*')) ? 'active' : '' }}">
-                      <i class="nav-icon fas fa-sliders-h"></i>
-                      <p>
-                        Settings
-                        <i class="fas fa-angle-left right"></i>
-                      </p>
-                    </a>
-                    <ul class="nav-item nav-treeview">
-                      <li class="nav-item">
-                        <a href="{{ route('user.index') }}" class="nav-link {{ (request()->is('settings/user*')) ? 'active' : '' }}">
-                            <i class="fas fa-users-cog"></i>
-                          <p>User</p>
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a href="{{ route('role.index') }}" class="nav-link {{ (request()->is('settings/role*')) ? 'active' : '' }}">
-                            <i class="fas fa-users-cog"></i>
-                          <p>Role</p>
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a href="{{ route('permission.index') }}" class="nav-link {{ (request()->is('settings/permission*')) ? 'active' : '' }}">
-                            <i class="fas fa-users-cog"></i>
-                          <p>Permission</p>
-                        </a>
-                      </li>
-                      <li class="nav-item">
-                        <a href="{{ route('setting.index') }}" class="nav-link {{ (request()->is('settings/setting*')) ? 'active' : '' }}">
-                            <i class="fas fa-users-cog"></i>
-                          <p>Setting</p>
-                        </a>
-                      </li>
-                    </ul>
-                </li>
-                @endhasanyrole
-                <br>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        <i class="fas fa-power-off nav-icon"></i>
-                        <p>
-                        {{ __('Logout') }}
-                        </p>
-                    </a>
+                            @hasanyrole('Super Admin|Admin')
+                            <li class="nav-item has-treeview {{ (request()->is('settings*')) ? 'menu-open' : '' }}">
+                                <a href="#" class="nav-link {{ (request()->is('settings*')) ? 'active' : '' }}">
+                                    <i class="nav-icon fas fa-sliders-h"></i>
+                                    <p>
+                                        Settings
+                                        <i class="fas fa-angle-left right"></i>
+                                    </p>
+                                </a>
+                                <ul class="nav-item nav-treeview">
+                                    <li class="nav-item">
+                                        <a href="{{ route('user.index') }}" class="nav-link {{ (request()->is('settings/user*')) ? 'active' : '' }}">
+                                            <i class="fas fa-users-cog"></i>
+                                        <p>User</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('role.index') }}" class="nav-link {{ (request()->is('settings/role*')) ? 'active' : '' }}">
+                                            <i class="fas fa-users-cog"></i>
+                                            <p>Role</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('permission.index') }}" class="nav-link {{ (request()->is('settings/permission*')) ? 'active' : '' }}">
+                                            <i class="fas fa-users-cog"></i>
+                                            <p>Permission</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('setting.index') }}" class="nav-link {{ (request()->is('settings/setting*')) ? 'active' : '' }}">
+                                            <i class="fas fa-users-cog"></i>
+                                            <p>Setting</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            @endhasanyrole
+                            <br>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    <i class="fas fa-power-off nav-icon"></i>
+                                    <p>
+                                        {{ __('Logout') }}
+                                    </p>
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </aside>
+        <div class="content-wrapper">
+            @yield('content')
 
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                        </form>
-                </li>
-            </ul>
-          </nav>
         </div>
-      </aside>
-      <div class="content-wrapper">
-          @yield('content')
-      </div>
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the right -->
+            <div class="float-right d-none d-sm-inline">
+            <!-- Dragokreatif 2019 -->
+            </div>
+            <!-- Default to the left -->
+            <strong>Copyright &copy; 2020 <a href="#">PT.Drago Digital Kreatifindo Reimbursement</a>.</strong>
+        </footer>
     </div>
-  </div>
+</div>
 
 <!-- jQuery -->
 <script src="{{ url('adminlte/plugins/jquery/jquery.min.js')}}"></script>
